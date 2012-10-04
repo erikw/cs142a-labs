ce3a46ebc3693a6d9be3b0af4812f479cb268b82
Erik Westrup <erik.westrup@gmail.com>
2012-10-03 17:08:35 -0700

    Basic test suit seems to work.

--- src/crux/Compiler.java
+++ src/crux/Compiler.java
@@ -16,6 +16,34 @@ public class Compiler {
     public static String studentID = "50471668";
     public static String uciNetID = "ewestrup";
 
+
+	/**
+	 * Compile the file give.
+	 * @param cruxFile The file to compile.
+	 */
+	public void compile(String cruxFile)
+	{
+        Scanner scanner = null;
+
+        try {
+            scanner = new Scanner(new FileReader(cruxFile));
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.err.println("Error accessing the source file: \"" + cruxFile + "\".");
+            System.exit(-2);
+        }
+
+        // TODO use iterable interface?
+        Token t = scanner.next();
+        while (t != null)
+        { /* TODO t is not the EOF token */
+            System.out.println(t);
+            t = scanner.next();
+        }
+        System.out.println(t);
+
+	}
+
 	/**
 	 * Main method that starts the compilation.
 	 *
@@ -26,22 +54,6 @@ public class Compiler {
 			System.err.println("One argument expected. Usage:\n $ java Compiler <source_file>");
         }
         String sourceFile = args[0];
-        Scanner scanner = null;
-
-        try {
-            scanner = new Scanner(new FileReader(sourceFile));
-        } catch (IOException e) {
-            e.printStackTrace();
-            System.err.println("Error accessing the source file: \"" + sourceFile + "\".");
-            System.exit(-2);
-        }
-
-        // TODO use iterable interface?
-        Token t = scanner.next();
-        while (t != null ) { /* TODO t is not the EOF token */
-            System.out.println(t);
-            t = scanner.next();
-        }
-        System.out.println(t);
+        new Compiler().compile(sourceFile);
     }
 }
--- src/test/TestFiles.java
+++ src/test/TestFiles.java
@@ -1,36 +1,156 @@
 package test;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FilenameFilter;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+
+import java.util.Scanner;
 
 import org.junit.*;
 import static org.junit.Assert.*;
+
+import crux.*;
 
 public class TestFiles {
 
 	/* Path to test files root dir. */
 	private static final String fileRoot = "tests";
 
-	public TestFiles() {
+	/* Compiler to use. */
+	private crux.Compiler compiler;
 
-	}
+	/* Original output stream. */
+	private PrintStream outOrg;
+
+	/* Original err stream. */
+	private PrintStream errOrg;
+
+	/* Buffer stdout stream. */
+	private ByteArrayOutputStream outBuffer;
+
+	/* Buffer stderr stream. */
+	private ByteArrayOutputStream errBuffer;
+
+	/* Our output stream. */
+	private PrintStream outStream;
+
+	/* Our err stream. */
+	private PrintStream errStream;
 
 	@Before
 	public void setUp() {
-
+		compiler = new crux.Compiler();	
+		outOrg = System.out;
+		errOrg = System.err;
+		outBuffer = new ByteArrayOutputStream();
+		errBuffer = new ByteArrayOutputStream();
+		outStream = new PrintStream(outBuffer);
+		errStream = new PrintStream(errBuffer);
 	}
 
 	@After
 	public void tearDown() {
-
+		compiler = null;
+		useOutBuffer(false);
 	}
 
 	@Test
-	public void testPublicFiles() {
-		File dir = new File(fileRoot + "/public");
-		String[] files = dir.list();
-		for (String file: files) {
-			System.out.println(file);
+	public void testPublic()
+	{
+		testFilesIn("public");
+	}
+
+	@Test
+	public void testPrivate()
+	{
+		testFilesIn("private");
+	}
+
+	private void testFilesIn(String subdir)
+	{
+		File dir = new File(fileRoot + "/" + subdir);
+		String[] cruxFiles = dir.list(new FilenameFilter() {
+ 	 	 	public boolean accept(File dir, String name) {
+ 	 	 		return name.matches(".*\\.crx$");
+ 	 	 	}
+		});
+
+		int nbrSucess = 0;
+		for (String cruxFile: cruxFiles)
+		{
+			String[] nameParts = cruxFile.split("\\.");
+			if (nameParts.length != 2)
+			{
+				System.err.println("File format changed?");
+				System.exit(1);
+			}
+			String outFile = nameParts[0] + ".out";
+			if (testFile(fileRoot + "/" + subdir + "/" + cruxFile, fileRoot + "/" + subdir + "/" + outFile))
+			{
+				++nbrSucess;
+			}
+		}
+		if (nbrSucess == cruxFiles.length)
+		{
+			System.out.println("All tests passed!");
+		} else
+		{
+			System.err.printf("%d/%d tests passed.\n", nbrSucess, cruxFiles.length);
 		}
 	}
 
+	/**
+	 * Compile input file and compare with reference.
+	 * @param cruxFileName The input crux file.
+	 * @param outFileName The reference expected output.
+	 */
+	private boolean testFile(String cruxFileName, String outFileName) 
+	{
+		System.out.printf("Testing input file \"%s\", with the expected output in \"%s\"\n", cruxFileName, outFileName);
+		useOutBuffer(false);
+		compiler.compile(cruxFileName);
+		String actual = outBuffer.toString();
+		String errStr = errBuffer.toString();
+		useOutBuffer(false);
+		if (!errStr.isEmpty())  {
+			System.err.printf("Compiler gave this error output: {\\n%s\\n}\\n", errStr);
+		}
+
+		File outFile = new File(outFileName);
+		java.util.Scanner outScanner = null;
+		try
+		{
+			outScanner = new java.util.Scanner(outFile);
+		} catch (FileNotFoundException fnfe)
+		{
+			System.err.printf("Bad filename \"%s\"!\n", outFileName);
+			return false;
+		}
+		String expected = outScanner.useDelimiter("\\Z").next();
+
+		assertEquals("Wrong compiler output.", expected, actual);
+
+		return true;
+	}
+
+	/**
+	 * Toggles the output buffers.
+	 * @param active If the buffers should be used or not.
+	 */
+	private void useOutBuffer(boolean active)
+	{
+		if (active)
+		{
+			System.setOut(outStream);
+			System.setErr(errStream);
+		} else
+		{
+			outBuffer.reset();
+			errBuffer.reset();
+			System.setOut(outOrg);
+			System.setErr(errOrg);
+		}
+	}
 }
