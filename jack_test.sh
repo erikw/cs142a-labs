24b06707f10403cbcd9bea39bfeee58ad5a80c1b
Erik Westrup <erik.westrup@gmail.com>
2012-12-02 16:44:01 -0800

    Added modded test script by TA.

---
+++ jack_test.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Run crux.Compiler on all the .crx files in the current directory, then
+# execute spim on the resulting assembly file with the .in file as input,
+# and lastly compare the result with the corresponding .out file.
+# Author: Jack Cheng
+
+for FILE in tests/lab6/public/*.crx
+do 
+	echo "Testing $FILE..."
+	java -cp bin: crux.Compiler $FILE > temp.asm
+	spim -file temp.asm < ${FILE%crx}in | sed '1,5 d' > temp.out
+	sed -e '$a\' ${FILE%crx}out > temp2.out
+	diff temp.out temp2.out --strip-trailing-cr
+	if [ "$?" -ne "0" ]; then 
+		break
+	fi
+done
+rm temp.asm temp.out temp2.out
